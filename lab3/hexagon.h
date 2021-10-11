253f35ed253daa2660632a25afabd11450b567c6
lumses <90564405+lumses@users.noreply.github.com>
2021-10-11 22:48:44 +0300

    Update hexagon.h

--- lab3/hexagon.h
+++ lab3/hexagon.h
@@ -3,22 +3,20 @@
 #include <iostream>
 #include "figure.h"
 #include "point.h"
-
+#include <memory>
 
 class Hexagon : public Figure {
 public:
     Hexagon();
     Hexagon(std::istream &is);
     Hexagon(Point a, Point b, Point c, Point d, Point e, Point f);
-    Hexagon(Hexagon &other);
+    Hexagon(std::shared_ptr<Hexagon>& other);
     double Area();
     size_t VertexesNumber();
     virtual ~Hexagon();
     Hexagon& operator=(const Hexagon& other);
     Hexagon& operator==(const Hexagon& other);
-    friend std::ostream& operator<<(std::ostream& os, Hexagon& h);
+    friend std::ostream& operator<<(std::ostream& os, std::shared_ptr<Hexagon>& h);
 private:
     Point a, b, c, d, e, f;
 };
-
-#endif // HEXAGON_H
