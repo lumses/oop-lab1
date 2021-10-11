d236cbceddd176a8a23a52bb0a207028504a0ed8
lumses <90564405+lumses@users.noreply.github.com>
2021-10-11 22:50:58 +0300

    Update and rename hlist.h to tlinkedlist.h

---
+++ lab3/tlinkedlist.h
@@ -0,0 +1,32 @@
+#ifndef HLIST_H
+#define HLIST_H
+#include <iostream>
+#include "hlist_item.h"
+#include "hexagon.h"
+
+class TLinkedList {
+public:
+  TLinkedList();
+  int size_of_list;
+  size_t Length();
+  void empty();
+  std::shared_ptr<Hexagon>& First();
+  std::shared_ptr<Hexagon>& Last();
+  std::shared_ptr<Hexagon>& GetItem(size_t idx);
+  void Empty();
+  void InsertFirst(const std::shared_ptr<Hexagon> &&hexagon);
+  void InsertLast(const std::shared_ptr<Hexagon> &&hexagon);
+  void RemoveLast();
+  void RemoveFirst();
+  void Insert(const std::shared_ptr<Hexagon> &&hexagon, size_t position);
+  void Remove(size_t position);
+  void Clear();
+  friend std::ostream& operator<<(std::ostream& os, TLinkedList& list);
+  ~TLinkedList(); 
+private:
+  std::shared_ptr<HListItem> front;
+  std::shared_ptr<HListItem> back;
+};
+ 
+#endif // HList_H
+
