fe942a925e3623e2a48ae5699c00eaa1cece94dd
lumses <90564405+lumses@users.noreply.github.com>
2021-10-11 22:49:28 +0300

    Update hlist_item.h

--- lab3/hlist_item.h
+++ lab3/hlist_item.h
@@ -1,12 +1,11 @@
-
 #include <iostream>
 #include "hexagon.h"
 #include <memory>
 
 class HListItem {
 public:
-  HListItem(std::shared_ptr<Hexagon> &hexagon);
-  friend std::ostream& operator<<(std::ostream& os, HListItem& obj);
+  HListItem(const std::shared_ptr<Hexagon> &hexagon);
+  friend std::ostream& operator<<(std::ostream& os, std::shared_ptr<HListItem>&& obj);
   ~HListItem();
   std::shared_ptr<Hexagon> hexagon;
   std::shared_ptr<HListItem> next;
