ac0893f707192e8be380daa91ecc64ce03df6d71
lumses <90564405+lumses@users.noreply.github.com>
2021-10-11 20:24:56 +0300

    Update main.cpp

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
-#include "hlist.h"
+#include "tlinkedlist.h"
 
 int main() {
-  HList hlist;
-  hlist.empty();
-  hlist.push_back(Hexagon(Point(1,2),Point(2,3),Point(3,4),Point(5,6),Point(7,8),Point(9,10)));
-  hlist.push_back(Hexagon(Point(11,12),Point(12,13),Point(13,14),Point(14,15),Point(15,16),Point(16,17)));
-  hlist.push_back(Hexagon(Point(17,18),Point(18,19),Point(19,20),Point(20,21),Point(21,22),Point(23,24)));
-  hlist.push_front(Hexagon(Point(17,18),Point(18,19),Point(19,20),Point(20,21),Point(21,22),Point(23,24)));
-  std::cout << hlist;
-  hlist.pop_back();
-  hlist.empty();
-  hlist.insert(Hexagon(Point(1,1),Point(2,3),Point(3,4),Point(5,6),Point(7,8),Point(9,16)),2);
-  hlist.erase(2);
-  std::cout << hlist;
-  hlist.clear();
+  TLinkedList tlinkedlist;
+  tlinkedlist.InsertLast(Hexagon(Point(1,2),Point(2,3),Point(3,4),Point(5,6),Point(7,8),Point(9,10)));
+  tlinkedlist.InsertLast(Hexagon(Point(11,12),Point(12,13),Point(13,14),Point(14,15),Point(15,16),Point(16,17)));
+  tlinkedlist.InsertLast(Hexagon(Point(17,18),Point(18,19),Point(19,20),Point(20,21),Point(21,22),Point(23,24)));
+  tlinkedlist.InsertLast(Hexagon(Point(17,18),Point(18,19),Point(19,20),Point(20,21),Point(21,22),Point(23,24)));
+  std::cout << tlinkedlist;
+  tlinkedlist.RemoveLast();
+  tlinkedlist.Insert(Hexagon(Point(1,1),Point(2,3),Point(3,4),Point(5,6),Point(7,8),Point(9,16)),2);
+  tlinkedlist.Remove(2);
+  std::cout << tlinkedlist;
+  tlinkedlist.Clear();
   return 0;
 }
